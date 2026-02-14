50c3ec5a01b0688475dd46acb344e206d3ee0241
Fluxxis <zurbenkoila4@gmail.com>
2026-02-15 00:12:20 +0100

    Delete api/sites.js

--- api/sites.js
+++
@@ -1,6 +0,0 @@
-import sites from "../sites.json" assert { type: "json" };
-
-export default function handler(req, res) {
-  res.setHeader("Cache-Control", "public, max-age=3600");
-  res.status(200).json({ count: sites.length, sites });
-}
